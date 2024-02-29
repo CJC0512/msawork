fdce1fe1f88044ec80ae4f000bd02a1504222aaf
CJC0512 <ehdchs2006@gmail.com>
2024-02-29 15:29:36 +0900

    chap01-01 server @EnableEurekaServer 추가

--- chap01-01-eureka-server-lecture-source/src/main/java/com/ohgiraffers/eurekaserver/Chap0101EurekaServerLectureSourceApplication.java
+++ chap01-01-eureka-server-lecture-source/src/main/java/com/ohgiraffers/eurekaserver/Chap0101EurekaServerLectureSourceApplication.java
@@ -2,8 +2,10 @@ package com.ohgiraffers.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Chap0101EurekaServerLectureSourceApplication {
 
     public static void main(String[] args) {
