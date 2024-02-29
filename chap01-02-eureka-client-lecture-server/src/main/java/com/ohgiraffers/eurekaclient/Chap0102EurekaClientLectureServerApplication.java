9b886dbddaed4d97e66b88eb8dcb59d2391f1335
CJC0512 <ehdchs2006@gmail.com>
2024-02-29 15:29:00 +0900

    chap01-02 eureka-client 생성

---
+++ chap01-02-eureka-client-lecture-server/src/main/java/com/ohgiraffers/eurekaclient/Chap0102EurekaClientLectureServerApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap0102EurekaClientLectureServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap0102EurekaClientLectureServerApplication.class, args);
+    }
+
+}
