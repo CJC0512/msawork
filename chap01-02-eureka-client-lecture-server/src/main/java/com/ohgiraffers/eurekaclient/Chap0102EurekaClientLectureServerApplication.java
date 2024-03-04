5aac4403fa2008e9193c9c1411a1055cb11f4641
CJC0512 <ehdchs2006@gmail.com>
2024-03-04 09:44:14 +0900

    chap01-02 client 추가 및 hello 작성

--- chap01-02-eureka-client-lecture-server/src/main/java/com/ohgiraffers/eurekaclient/Chap0102EurekaClientLectureServerApplication.java
+++ chap01-02-eureka-client-lecture-server/src/main/java/com/ohgiraffers/eurekaclient/Chap0102EurekaClientLectureServerApplication.java
@@ -2,8 +2,12 @@ package com.ohgiraffers.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
+/* 설명. 유라케 서버의 클라이언트로 등록할 프로젝트로 기동 */
+@EnableDiscoveryClient
 public class Chap0102EurekaClientLectureServerApplication {
 
     public static void main(String[] args) {
