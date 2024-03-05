aaffd604a8e5a84cbd2e751e5fe27c09f3c29e12
CJC0512 <ehdchs2006@gmail.com>
2024-03-05 13:53:18 +0900

    security 보안 적용

--- chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/service/UserService.java
+++ chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/service/UserService.java
@@ -2,7 +2,8 @@ package com.ohgiraffers.userservice.service;
 
 
 import com.ohgiraffers.userservice.dto.UserDTO;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     void registUser(UserDTO userDTO);
 }
--- chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/service/UserServiceImpl.java
+++ chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/service/UserServiceImpl.java
@@ -5,16 +5,19 @@ import com.ohgiraffers.userservice.dto.UserDTO;
 import com.ohgiraffers.userservice.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
     private ModelMapper modelMapper;
@@ -47,5 +50,20 @@ public class UserServiceImpl implements UserService{
 
         userRepository.save(userEntity);
 
+    }
+
+    /* 설명. UserDetailsService 인터페이스 상속 이후 DB에서 로그인 사용자 정보 조회 후 UserDetails 타입으로 반환하는 기능 구현 */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        UserEntity userEntity = userRepository.findByEmail(email);
+
+        if (userEntity == null)
+            throw new UsernameNotFoundException(email + "아이디의 유저는 존재하지 않음");
+
+        /* 설명. Security 의 User() */
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(),
+                true, true, true, true,
+                new ArrayList<>());
     }
 }
